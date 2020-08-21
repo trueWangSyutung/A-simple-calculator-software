477cbc34b919330b0ed4b12fe702c4e2460c1354
Syutung2 <wangxudong@syutung.cn>
2020-08-21 18:16:14 +0800

    界面一点点修改
    界面一点点修改

--- src/main/java/com/example/myapplication/About.kt
+++ src/main/java/com/example/myapplication/About.kt
@@ -1,17 +1,63 @@
 package com.example.myapplication
 
 import android.content.Intent
+import android.graphics.Color
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_about.*
 
 class About : AppCompatActivity() {
+    protected var useThemestatusBarColor = false
+    protected var useStatusBarColor = true
+    protected fun setStatusBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) { //5.0及以上
+            val decorView: View = window.decorView
+            val option: Int = (View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
+            decorView.setSystemUiVisibility(option)
+            //根据上面设置是否对状态栏单独设置颜色
+            if (useThemestatusBarColor) {
+                window.statusBarColor = resources.getColor(android.R.color.background_light) //设置状态栏背景色
+            } else {
+                window.statusBarColor = Color.TRANSPARENT //透明
+            }
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) { //4.4到5.0
+            val localLayoutParams = window.attributes
+            localLayoutParams.flags =
+                WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS or localLayoutParams.flags
+        } else {
+            Toast.makeText(this, "低于4.4的android系统版本不存在沉浸式状态栏", Toast.LENGTH_SHORT).show()
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && useStatusBarColor) { //android6.0以后可以对状态栏文字颜色和图标进行修改
+            window.decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
+    }
+    protected fun hideBottomUIMenu() {
+        //隐藏虚拟按键，并且全屏
+        if (Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) { // lower api
+            val v = this.window.decorView
+            v.systemUiVisibility = View.GONE
+        } else if (Build.VERSION.SDK_INT >= 19) {
+            //for new api versions.
+            val decorView = window.decorView
+            val uiOptions = (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_FULLSCREEN)
+            decorView.systemUiVisibility = uiOptions
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
 
+        hideBottomUIMenu()
+        setStatusBar()
         kaiyuan.setOnClickListener {
             val intent = Intent(Intent.ACTION_VIEW)
             intent.data = Uri.parse("https://github.com/Syutung2/A-simple-calculator-software")
--- src/main/java/com/example/myapplication/MainActivity.kt
+++ src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,23 +1,42 @@
 package com.example.myapplication
 
+import android.R
 import android.app.Service
 import android.content.Intent
-import android.content.res.Configuration
+import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.os.Vibrator
+import android.view.View
+import android.view.WindowManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
 
+class MainActivity : AppCompatActivity() {
+    protected var useThemestatusBarColor = false
+    protected var useStatusBarColor = true
+    protected fun hideBottomUIMenu() {
+        //隐藏虚拟按键，并且全屏
+        if (Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) { // lower api
+            val v = this.window.decorView
+            v.systemUiVisibility = View.GONE
+        } else if (Build.VERSION.SDK_INT >= 19) {
+            //for new api versions.
+            val decorView = window.decorView
+            val uiOptions = (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_FULLSCREEN)
+            decorView.systemUiVisibility = uiOptions
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(com.example.myapplication.R.layout.activity_main)
         //当系统版本为4.4或者4.4以上时可以使用沉浸式状态栏
 
-
+        hideBottomUIMenu()
+        setStatusBar()
         var numxiao1=0.0
         var numxiao2=0.0
         var have_xiaoshudian=false
@@ -436,6 +455,31 @@ class MainActivity : AppCompatActivity() {
 
                 }
             }
+        }
+    }
+
+    protected fun setStatusBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) { //5.0及以上
+            val decorView: View = window.decorView
+            val option: Int = (View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
+            decorView.setSystemUiVisibility(option)
+            //根据上面设置是否对状态栏单独设置颜色
+            if (useThemestatusBarColor) {
+                window.statusBarColor = resources.getColor(R.color.background_light) //设置状态栏背景色
+            } else {
+                window.statusBarColor = Color.TRANSPARENT //透明
+            }
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) { //4.4到5.0
+            val localLayoutParams = window.attributes
+            localLayoutParams.flags =
+                WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS or localLayoutParams.flags
+        } else {
+            Toast.makeText(this, "低于4.4的android系统版本不存在沉浸式状态栏", Toast.LENGTH_SHORT).show()
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && useStatusBarColor) { //android6.0以后可以对状态栏文字颜色和图标进行修改
+            window.decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
         }
     }
 }
