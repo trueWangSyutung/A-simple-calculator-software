2f11b12265edbf73154ba176a4967b43b6d680ce
Syutung2 <wangxudong@syutung.cn>
2020-08-22 19:43:37 +0800

    添加横屏优化、更改app名称、优化界面布局

---
+++ src/main/java/cn/syutung/jisuanqi/More.kt
@@ -0,0 +1,58 @@
+package cn.syutung.jisuanqi
+
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import android.widget.Toast
+import com.example.myapplication.R
+import kotlinx.android.synthetic.main.activity_more.*
+
+class More : AppCompatActivity() {
+    protected var useThemestatusBarColor = false
+    protected var useStatusBarColor = true
+    protected fun setStatusBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) { //5.0及以上
+            val decorView: View = window.decorView
+            val option: Int = (View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
+            decorView.setSystemUiVisibility(option)
+            //根据上面设置是否对状态栏单独设置颜色
+            if (useThemestatusBarColor) {
+                window.statusBarColor = resources.getColor(android.R.color.background_light) //设置状态栏背景色
+            } else {
+                window.statusBarColor = Color.TRANSPARENT //透明
+            }
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) { //4.4到5.0
+            val localLayoutParams = window.attributes
+            localLayoutParams.flags =
+                WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS or localLayoutParams.flags
+        } else {
+            Toast.makeText(this, "低于4.4的android系统版本不存在沉浸式状态栏", Toast.LENGTH_SHORT).show()
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && useStatusBarColor) { //android6.0以后可以对状态栏文字颜色和图标进行修改
+            window.decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_more)
+
+        setStatusBar()
+        jinzhi.setOnClickListener {
+            val intant = Intent(this, JinZhi::class.java)
+            startActivity(intant)
+        }
+        jisuanqi.setOnClickListener {
+            val intant = Intent(this, MainActivity::class.java)
+            startActivity(intant)
+
+        }
+
+
+    }
+}
