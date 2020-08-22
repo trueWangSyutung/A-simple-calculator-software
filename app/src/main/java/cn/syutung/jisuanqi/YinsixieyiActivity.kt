5c98a98d63029248cd18ef3fdb7e81aecba2ae3b
Syutung2 <wangxudong@syutung.cn>
2020-08-23 04:24:37 +0800

    添加Project下所有文件，增加英文和繁体翻译

---
+++ app/src/main/java/cn/syutung/jisuanqi/YinsixieyiActivity.kt
@@ -0,0 +1,21 @@
+package cn.syutung.jisuanqi
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.res.Configuration
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.textclassifier.TextClassifierEvent
+import com.example.myapplication.R
+import kotlinx.android.synthetic.main.activity_yinsixieyi.*
+import java.util.*
+
+class YinsixieyiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_yinsixieyi)
+
+
+    }
+}
